206131760ba6f53c4262c8b1838eef355466ae9e
Robert Blackburn <rob@blackburnlabs.com>
2025-10-15 16:09:19 -0400

    feat: Integrate React Redux Provider in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import { AppContent } from './components/AppContent';
+import { store } from './store';
 import theme from './theme/theme';
 
 function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <BrowserRouter>
-                <AppContent />
-            </BrowserRouter>
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <BrowserRouter>
+                    <AppContent />
+                </BrowserRouter>
+            </ThemeProvider>
+        </Provider>
     );
 }
 
